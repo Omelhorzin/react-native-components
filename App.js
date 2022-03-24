89ba5ee7cb630e1760178402ec5d0ab013b14e07
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-03-24 20:05:19 -0300

    style: adding style

--- App.js
+++ App.js
@@ -15,9 +15,13 @@ export default function App() {
         <Text style={styles.title}>Estácio</Text>
       </View>
       <View style={styles.detailsView}>
-        <Text>Xbox Series X</Text>
-        <Image source={{ uri: 'https://arquivosbordados.com/wp-content/uploads/2021/10/embroidery-design-logo_753eeb22-e242-4204-bf27-f3012cc27322-1.jpg' }} style={styles.header}></Image>
-        <Text>Sony</Text>
+        <Text style={styles.productName}>Xbox Series X</Text>
+      </View>
+      <View style={styles.detailsContainer}>
+        <Image style={styles.icon} source={{ uri: 'https://arquivosbordados.com/wp-content/uploads/2021/10/embroidery-design-logo_753eeb22-e242-4204-bf27-f3012cc27322-1.jpg' }}></Image>
+        <Text style={styles.sonyName}>Sony</Text>
+      </View>
+      <View>
         <Text>Lorem ipsum dolor sit amet conseqt adiscipli elit</Text>
         <Text>R$ 4.0000,00</Text>
       </View>
@@ -42,14 +46,37 @@ const styles = StyleSheet.create({
   header: {
     width: '100%',
     height: 350,
-  }, 
+  },
   title: {
     fontSize: 30,
     fontWeight: 'bold',
     color: '#6b5b95',
-    paddingTop: 30,
+    paddingTop: 40,
     position: 'absolute',
     textAlign: 'center',
     width: '100%',
+  },
+  productName: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#6b5b95',
+    textAlign: 'center',
+    width: '100%',
+  },
+  icon: {
+    width: 32,
+    height: 32,
+  },
+  sonyName: {
+    fontSize: 15,
+    color: '#6b5b95',
+    marginLeft: 20,
+    lineHeight: 10,
+    paddingTop: 10,
+  },
+  detailsContainer: {
+    padding: 20,
+    flexDirection: 'row'
+    // paddingVertical: 10,
   }
 });
