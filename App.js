d5ef5434b374ffcf338c4de34cd494580b1b2e04
Miqueias Maia <miqueiasmaia7@gmail.com>
2022-03-31 18:51:21 -0300

    feat: add style to main page

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView, Image } from 'react-native';
-import { Dimensions, View } from 'react-native';
+import { 
+  StyleSheet, Text, SafeAreaView, Image, Button, Dimensions, View
+} from 'react-native';
 
 import header from './assets/header.png';
-
-const width = Dimensions.get('window').width;
-const height = Dimensions.get('window').height;
 
 export default function App() {
   return (
     <SafeAreaView>
       <View style={styles.headerView}>
         <Image source={header} style={styles.header} ></Image>
-        <Text style={styles.title}>Estácio</Text>
+        <Text style={styles.title}>Consoles</Text>
       </View>
       <View style={styles.detailsView}>
         <Text style={styles.productName}>Xbox Series X</Text>
@@ -22,8 +20,17 @@ export default function App() {
         <Text style={styles.sonyName}>Sony</Text>
       </View>
       <View>
-        <Text>Lorem ipsum dolor sit amet conseqt adiscipli elit</Text>
-        <Text>R$ 4.0000,00</Text>
+        <Text style={styles.productDescription}>Lorem ipsum dolor sit amet conseqt adiscipli elit. Lorem ipsum dolor sit amet conseqt adiscipli elit.Lorem ipsum dolor sit amet conseqt adiscipli elit.Lorem ipsum dolor sit amet conseqt adiscipli elit.Lorem ipsum dolor sit amet conseqt adiscipli elit.</Text>
+      </View>
+      <View>
+        <Text style={styles.productPrice}>R$ 4.0000,00</Text>
+      </View>
+      <View style={styles.cartArea}>
+        <Button
+          title="Adicionar ao carrinho"
+          color="#007AFF"
+          accessibilityLabel="Adicionar ao carrinho"
+        />
       </View>
     </SafeAreaView>
   );
@@ -46,11 +53,15 @@ const styles = StyleSheet.create({
   header: {
     width: '100%',
     height: 350,
+    borderBottomColor: '#007AFF',
+    borderBottomWidth: 5,
+    borderBottomStartRadius: 20,
+    borderBottomEndRadius: 20,
   },
   title: {
     fontSize: 30,
     fontWeight: 'bold',
-    color: '#6b5b95',
+    color: '#1a42a7',
     paddingTop: 40,
     position: 'absolute',
     textAlign: 'center',
@@ -58,9 +69,9 @@ const styles = StyleSheet.create({
   },
   productName: {
     fontSize: 30,
+    paddingLeft: 10,
     fontWeight: 'bold',
-    color: '#6b5b95',
-    textAlign: 'center',
+    color: '#1a42a7',
     width: '100%',
   },
   icon: {
@@ -77,6 +88,23 @@ const styles = StyleSheet.create({
   detailsContainer: {
     padding: 20,
     flexDirection: 'row'
-    // paddingVertical: 10,
+  },
+  productDescription: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    fontSize: 15,
+    color: '#6b5b95',
+    textAlign: 'justify',
+  },
+  productPrice: {
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingTop: 10,
+    fontSize: 30,
+    color: '#106510',
+    fontWeight: 'bold',
+  }, 
+  cartArea: {
+    padding: 20
   }
 });
